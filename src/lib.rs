ec08ba142afd05a3cbca2b070a817cc662d2a541
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2021-04-04 23:19:17 +0200

    Use REPOS_DIR for base

--- src/lib.rs
+++ src/lib.rs
@@ -14,49 +14,45 @@
 
 use url::Url;
 
-const FILTER: [&str; 1] = ["git"];
-
 /// Transform the url into the folder structure.
 /// This is used for the target folder of the git clone command.
-pub fn organize(prefix: &str, url: &str) -> String {
-    let url = url.trim();
-    let url = url.strip_suffix(".git").unwrap_or_else(|| &url);
+pub fn to_filesystem_path(base: &str, url: &str) -> String {
+    let url = trim(url);
     if let Ok(url) = Url::parse(url) {
-        parse_https_url(url, prefix)
+        parse_https_url(url, base)
     } else {
-        parse_ssh_url(url, prefix)
+        parse_ssh_url(url, base)
     }
 }
 
-fn parse_https_url(url: Url, prefix: &str) -> String {
-    let path: String = url
-        .path()
-        .split('/')
-        .filter(|el| !FILTER.contains(el))
-        .collect::<Vec<_>>()
-        .join("/");
-    format!("{}/{}{}", prefix, url.host_str().unwrap(), path)
+fn trim(url: &str) -> &str {
+    let url = url.trim();
+    let url = url.strip_suffix(".git").unwrap_or_else(|| &url);
+    url
 }
 
-fn parse_ssh_url(url: &str, prefix: &str) -> String {
+fn parse_https_url(url: Url, base: &str) -> String {
+    let path: String = url.path().split('/').collect::<Vec<_>>().join("/");
+    format!("{}/{}{}", base, url.host_str().unwrap(), path)
+}
+
+fn parse_ssh_url(url: &str, base: &str) -> String {
     // FORGIVE ME: There is no ssh formatting validation
+    let url = trim_ssh(url);
+
     // ["git@github.com", "lihram/git-clown-rs"]
     let ssh_parts = url.split(':').collect::<Vec<_>>();
 
     // ["git", "github.com"] -> "github.com"
     let domain = ssh_parts[0].split('@').collect::<Vec<_>>()[1];
 
-    let path: String = path_filtered(ssh_parts[1]);
+    let path = ssh_parts[1];
 
-    format!("{}/{}/{}", prefix, domain, path)
+    format!("{}/{}/{}", base, domain, path)
 }
 
-/// Remove parts of the path which are found in FILTER.
-fn path_filtered(path: &str) -> String {
-    path.split('/')
-        .filter(|el| !FILTER.contains(el))
-        .collect::<Vec<_>>()
-        .join("/")
+fn trim_ssh(url: &str) -> &str {
+    url.strip_prefix("ssh://").unwrap_or_else(|| &url)
 }
 
 #[cfg(test)]
@@ -66,7 +62,7 @@ mod tests {
     fn github_https() {
         let url = "https://github.com/lihram/rust-clone-organized.git";
         let expected = "git/github.com/lihram/rust-clone-organized";
-        let actual = organize("git", url);
+        let actual = to_filesystem_path("git", url);
 
         assert_eq!(actual, expected);
     }
@@ -75,14 +71,8 @@ mod tests {
     fn github_ssh() {
         let url = "git@github.com:lihram/rust-clone-organized.git";
         let expected = "git/github.com/lihram/rust-clone-organized";
-        let actual = organize("git", url);
+        let actual = to_filesystem_path("git", url);
 
         assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn filter_git() {
-        let path = path_filtered("/hello/git/world");
-        assert_eq!("/hello/world", &path);
     }
 }
